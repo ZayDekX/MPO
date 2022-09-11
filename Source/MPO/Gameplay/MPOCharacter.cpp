cd0cbc0d08a6e6d9d7f6457a0e2e3a5d8c706067
ZayDekX <zaydekx@gmail.com>
2022-09-12 04:39:46 +0700

    Separate player character and generic character classes

--- Source/MPO/Gameplay/MPOCharacter.cpp
+++ Source/MPO/Gameplay/MPOCharacter.cpp
@@ -1,15 +1,10 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "MPOCharacter.h"
-#include "UObject/ConstructorHelpers.h"
-#include "Camera/CameraComponent.h"
-#include "Components/DecalComponent.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/CharacterEquipment.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/PlayerController.h"
-#include "GameFramework/SpringArmComponent.h"
-#include "Materials/Material.h"
 #include "Engine/World.h"
 #include "Kismet/KismetMathLibrary.h"
 #include "Components/HealthComponent.h"
@@ -30,31 +25,6 @@ AMPOCharacter::AMPOCharacter()
     GetCharacterMovement()->RotationRate = FRotator(0.f, 640.f, 0.f);
     GetCharacterMovement()->bConstrainToPlane = true;
     GetCharacterMovement()->bSnapToPlaneAtStart = true;
-
-    // Create a camera boom...
-    CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
-    CameraBoom->SetupAttachment(RootComponent);
-    CameraBoom->SetUsingAbsoluteRotation(true); // Don't want arm to rotate when character does
-    CameraBoom->TargetArmLength = 800.f;
-    CameraBoom->SetRelativeRotation(FRotator(-30.f, 0.f, 0.f));
-    CameraBoom->bDoCollisionTest = false; // Don't want to pull camera in when it collides with level
-    //CameraBoom->bUsePawnControlRotation = true;
-
-    // Create a camera...
-    TopDownCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("TopDownCamera"));
-    TopDownCameraComponent->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
-    TopDownCameraComponent->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
-
-    // Create a decal in the world to show the cursor's location
-    CursorToWorld = CreateDefaultSubobject<UDecalComponent>("CursorToWorld");
-    CursorToWorld->SetupAttachment(RootComponent);
-    static ConstructorHelpers::FObjectFinder<UMaterial> DecalMaterialAsset(TEXT("Material'/Game/TopDownCPP/Blueprints/M_Cursor_Decal.M_Cursor_Decal'"));
-    if (DecalMaterialAsset.Succeeded())
-    {
-        CursorToWorld->SetDecalMaterial(DecalMaterialAsset.Object);
-    }
-    CursorToWorld->DecalSize = FVector(16.0f, 32.0f, 32.0f);
-    CursorToWorld->SetRelativeRotation(FRotator(90.0f, 0.0f, 0.0f).Quaternion());
 
     // Create inventory
     CharacterInventory = CreateDefaultSubobject<UInventoryComponent>(TEXT("InventoryComponent"));
@@ -99,21 +69,13 @@ void AMPOCharacter::Tick(float DeltaSeconds)
 
     IsMoving = GetVelocity().Size() > 0;
 
-    auto PlayerController = GetController<AMPOPlayerController>();
+    LookAt = GetLookAtPoint();
     
-    if (!PlayerController)
-    {
-        LookAt = GetActorForwardVector();
-    }
-    else
-    {
-        LookAt = PlayerController->PointUnderCursorLocation;
-
-        CursorToWorld->SetWorldLocation(PlayerController->PointUnderCursorLocation);
-        CursorToWorld->SetWorldRotation(PlayerController->UnderCursorSufaceNormal.Rotation());
-    }
-
     auto LookAtRotation = UKismetMathLibrary::FindLookAtRotation(GetActorLocation(), LookAt);
     auto Delta = UKismetMathLibrary::RInterpTo(GetActorRotation(), LookAtRotation, DeltaSeconds, 10) - GetActorRotation();
     AddActorWorldRotation(FRotator(0, Delta.Yaw, 0));
+}
+
+FVector AMPOCharacter::GetLookAtPoint() {
+    return GetActorForwardVector();
 }
